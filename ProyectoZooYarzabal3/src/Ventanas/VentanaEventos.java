69299aecfc5cc6ef1c4cf4db3ad894fff99f4cc3
ikerariasmartinez <ikerariasmartinez@192.168.1.146>
2024-01-03 21:02:15 +0100

    Ventana eventos

--- ProyectoZooYarzabal3/src/Ventanas/VentanaEventos.java
+++ ProyectoZooYarzabal3/src/Ventanas/VentanaEventos.java
@@ -1,9 +1,12 @@
 package Ventanas;
 
 import java.awt.BorderLayout;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -15,13 +18,13 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
-import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+// import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 
 public class VentanaEventos extends JFrame{
 	private JFrame vActual, vAnterior;
 	private JButton btnVolver;
 	private JPanel pSur;
-	private EmbeddedMediaPlayerComponent component;
+	// private EmbeddedMediaPlayerComponent component;
 	
 	public VentanaEventos () { //JFrame va
 		super();
@@ -41,7 +44,7 @@ public class VentanaEventos extends JFrame{
 		 */
 		
 		JTabbedPane tabbedPane = new JTabbedPane();
-		
+
 		// Evento Mundo Marino
 		JPanel panelEvento1 = new JPanel(new GridLayout(3,1));
 		panelEvento1.setBackground(new Color(70, 130, 180));
@@ -55,9 +58,25 @@ public class VentanaEventos extends JFrame{
 		panelEvento1.add(labelTitulo, BorderLayout.NORTH);
 		JPanel panelVideo = new JPanel();
 		panelVideo.setBackground(Color.black);
-		panelEvento1.add(panelVideo, BorderLayout.CENTER);
-		JTextArea area = new JTextArea("asdfasdf");
-		panelEvento1.add(new JScrollPane(area));
+		panelEvento1.add(panelVideo, BorderLayout.CENTER); 
+		
+		
+		JTextArea area = new JTextArea();
+		area.setEditable(false);
+		area.setLineWrap(true);
+		area.setWrapStyleWord(true);
+		area.setText("¡Maravillas Acuáticas en Vivo!\n\n" +
+                "Sumérgete en la fascinante maravilla del mundo marino con nuestra actuación exclusiva en el Zoológico Oceanográfico. Prepárate para una experiencia única, donde la gracia y la destreza de nuestras criaturas acuáticas te cautivarán.\n\n" +
+                "Detalles del Show:\n" +
+                "🐬 Actuaciones Asombrosas: Observa a nuestras estrellas marinas en su esplendor mientras realizan impresionantes acrobacias y demostraciones de agilidad. Desde saltos elegantes hasta juegos interactivos, te sorprenderás con su inteligencia.\n\n" +
+                "🌊 Educación y Conservación: Descubre más sobre la vida marina mientras nuestros expertos comparten información valiosa sobre la conservación de estas especies asombrosas. Tu participación contribuye a la preservación de los océanos y sus habitantes.\n\n" +
+                "📅 Fechas y Horarios (Enero) \n" +
+                "Sesión 1: 02-01.2024 - 13:30\n" +
+                "Sesión 2: 17-01-2024 - 10:00 \n\n" +
+                "Entradas:\n" +
+                "Adquiere tus entradas ahora y asegura tu lugar para este espectáculo único. ¡No te pierdas la oportunidad de sumergirte en la magia del océano en vivo!");
+		
+		panelEvento1.add(new JScrollPane(area)); 
 		
 		JPanel panelEvento2 = new JPanel(new GridLayout(3,1));
 		panelEvento2.setBackground(new Color(70, 130, 180));
@@ -72,8 +91,27 @@ public class VentanaEventos extends JFrame{
 		JPanel panelVideo2 = new JPanel();
 		panelVideo2.setBackground(Color.black);
 		panelEvento2.add(panelVideo2, BorderLayout.CENTER);
-		JTextArea area2 = new JTextArea("asdfasdf");
+		
+		
+		JTextArea area2 = new JTextArea();
+		area2.setEditable(false);
+		area2.setLineWrap(true);
+		area2.setWrapStyleWord(true);
+		area2.setText("¡Majestuosos Leones en Acción!\n\n" +
+                "Adéntrate en el emocionante mundo de la realeza animal con nuestro espectáculo exclusivo de leones. Prepárate para una experiencia única, donde la majestuosidad y el poder de estos felinos te dejarán sin aliento.\n\n" +
+                "Detalles del Show:\n" +
+                "🦁 Actuaciones Impresionantes: Observa a nuestros leones en su máximo esplendor mientras realizan increíbles acrobacias y demostraciones de fuerza. Desde saltos elegantes hasta demostraciones de destreza, te asombrarás con su agilidad.\n\n" +
+                "🌍 Educación y Conservación: Descubre más sobre la vida y el comportamiento de los leones mientras nuestros expertos comparten información valiosa sobre la conservación de esta especie majestuosa. Tu participación contribuye a la preservación de estos magníficos animales.\n\n" +
+                "📅 Fechas y Horarios (Enero) :\n" +
+                "12-01-2024 - 13-01-2024 - 20-01-2024\n" +
+                "12:30\n\n" +
+                "Entradas:\n" +
+                "Adquiere tus entradas ahora y asegura tu lugar para este espectáculo único. ¡No te pierdas la oportunidad de presenciar la grandeza de los leones en vivo!\n\n" +
+                "Información de Contacto:\n" +
+                "¡Únete a nosotros para un día inolvidable lleno de gracia, poder y asombro en el increíble espectáculo de leones!");
+		
 		panelEvento2.add(new JScrollPane(area2));
+		
 		
 		JPanel panelEvento3 = new JPanel(new GridLayout(3,1));
 		panelEvento3.setBackground(new Color(70, 130, 180));
@@ -88,8 +126,25 @@ public class VentanaEventos extends JFrame{
 		JPanel panelVideo3 = new JPanel();
 		panelVideo3.setBackground(Color.black);
 		panelEvento3.add(panelVideo3, BorderLayout.CENTER);
-		JTextArea area3 = new JTextArea("asdfasdf");
-		panelEvento3.add(new JScrollPane(area3));
+		
+		JTextArea area3 = new JTextArea();
+		area3.setEditable(false);
+		area3.setLineWrap(true);
+		area3.setWrapStyleWord(true);
+		area3.setText("¡Aventura Ártica en Directo!\n\n" +
+                "Embárcate en una emocionante travesía polar con nuestro espectáculo exclusivo en el Zoológico Frígido. Prepárate para una experiencia única, donde la majestuosidad y la gracia de nuestras criaturas polares te dejarán maravillado.\n\n" +
+                "Detalles del Show:\n" +
+                "❄️ Actuaciones Impresionantes: Observa a nuestros habitantes del frío en su máximo esplendor mientras realizan increíbles acrobacias y demostraciones de agilidad. Desde giros elegantes hasta demostraciones de fuerza, te sorprenderás con su resistencia al clima extremo.\n\n" +
+                "🌐 Educación y Conservación: Descubre más sobre la vida en el Ártico mientras nuestros expertos comparten información valiosa sobre la conservación de estas especies fascinantes. Tu participación contribuye a la preservación de estos increíbles animales y sus hábitats únicos.\n\n" +
+                "📅 Fechas y Horarios:\n" +
+                "Dias: Todos los viernes y sabados\n" +
+                "Función 2: 15:00\n\n" +
+                "Entradas:\n" +
+                "Adquiere tus entradas ahora y asegura tu lugar para este espectáculo polar único. ¡No te pierdas la oportunidad de sumergirte en la maravilla del Ártico en vivo!\n\n" +
+                "¡Únete a nosotros para un día inolvidable lleno de aventura polar, conocimiento y asombro en el espectáculo del Zoológico Frígido!");
+			
+			
+			panelEvento3.add(new JScrollPane(area3));
 		
 		JPanel panelEvento4 = new JPanel(new GridLayout(3,1));
 		panelEvento4.setBackground(new Color(70, 130, 180));
@@ -104,8 +159,27 @@ public class VentanaEventos extends JFrame{
 		JPanel panelVideo4 = new JPanel();
 		panelVideo4.setBackground(Color.black);
 		panelEvento4.add(panelVideo4, BorderLayout.CENTER);
-		JTextArea area4 = new JTextArea("asdfasdf");
-		panelEvento4.add(new JScrollPane(area4));
+		
+		JTextArea area4 = new JTextArea();
+		area4.setEditable(false);
+		area4.setLineWrap(true);
+		area4.setWrapStyleWord(true);
+		area4.setText("¡El Hábitat del Tigre en Vivo!\n\n" +
+                "Adéntrate en el misterioso mundo del hábitat del tigre con nuestro espectáculo exclusivo en el Zoológico Salvaje. Prepárate para una experiencia única, donde la elegancia y el poder de estos felinos te transportarán a su hábitat natural.\n\n" +
+                "Detalles del Show:\n" +
+                "🐅 Actuaciones Impactantes: Observa a nuestros tigres en su máximo esplendor mientras realizan impresionantes acrobacias y demostraciones de agilidad. Desde saltos majestuosos hasta demostraciones de destreza, te asombrarás con su fuerza y gracia.\n\n" +
+                "🌳 Educación y Conservación: Descubre más sobre la vida de los tigres y la importancia de preservar su hábitat mientras nuestros expertos comparten información valiosa. Tu participación contribuye a la conservación de estas magníficas criaturas y sus hogares naturales.\n\n" +
+                "📅 Fechas y Horarios:\n" +
+                "Presentación 1: 10-01-2024 13:00\n" +
+                "Presentación 2: 12-01-2024 13:00 \n" +
+                "Presentación 3: 16-01-2024 13:00 \n" +
+                "Presentación 4: 23-01-2024 14:00 \n\n" +
+                "Entradas:\n" +
+                "Adquiere tus entradas ahora y asegura tu lugar para este espectáculo único en el Zoológico Salvaje. ¡No te pierdas la oportunidad de sumergirte en la majestuosidad del hábitat del tigre en vivo!\n\n" +
+                "¡Únete a nosotros para un día inolvidable lleno de maravilla, conocimiento y asombro en el espectáculo \"El Hábitat del Tigre\" del Zoológico Salvaje!");
+			
+			
+			panelEvento4.add(new JScrollPane(area4));
 		
 		JPanel panelEvento5 = new JPanel(new GridLayout(3,1));
 		panelEvento5.setBackground(new Color(70, 130, 180));
@@ -120,7 +194,23 @@ public class VentanaEventos extends JFrame{
 		JPanel panelVideo5 = new JPanel();
 		panelVideo5.setBackground(Color.black);
 		panelEvento5.add(panelVideo5, BorderLayout.CENTER);
-		JTextArea area5 = new JTextArea("asdfasdf");
+		
+		JTextArea area5 = new JTextArea();
+		area5.setEditable(false);
+		area5.setLineWrap(true);
+		area5.setWrapStyleWord(true);
+		area5.setText("¡Tortugas en Acción: Un Viaje Maravilloso!\n\n" +
+                "Embárcate en un fascinante viaje marino con nuestro espectáculo exclusivo \"Tortugas en Acción\" en el Zoológico Oceanográfico. Prepárate para una experiencia única, donde la gracia y la vitalidad de estas criaturas marinas te sumergirán en un mundo lleno de asombro.\n\n" +
+                "Detalles del Show:\n" +
+                "🐢 Actuaciones Encantadoras: Observa a nuestras tortugas en su máximo esplendor mientras realizan impresionantes acrobacias y demostraciones de agilidad en el agua. Desde elegantes movimientos hasta juegos interactivos, te sorprenderás con su destreza.\n\n" +
+                "🌊 Educación y Conservación: Descubre más sobre la vida de las tortugas marinas y la importancia de su conservación mientras nuestros expertos comparten información valiosa. Tu participación contribuye a la preservación de estas especies y la protección de los océanos.\n\n" +
+                "📅 Fechas y Horarios:\n" +
+                "Dias: Todos los Lunes y Viernes\n" +
+                "Horario: 14:00 \n\n" +
+                "Entradas:\n" +
+                "Adquiere tus entradas ahora y asegura tu lugar para este espectáculo único en el Zoológico Oceanográfico. ¡No te pierdas la oportunidad de ser testigo de las \"Tortugas en Acción\" en vivo!\n\n" +
+                "¡Únete a nosotros para un día inolvidable lleno de maravillas acuáticas, aprendizaje y asombro en el espectáculo de \"Tortugas en Acción\" del Zoológico Oceanográfico!");
+			
 		panelEvento5.add(new JScrollPane(area5));
 		
 		tabbedPane.addTab("Mundo Marino", panelEvento1);
@@ -148,8 +238,8 @@ public class VentanaEventos extends JFrame{
 	}
 	//Informacion de todos los eventos del zoo 
 	public void lanzarVideo (String urlVideo) {
-		component.mediaPlayer().audio().setVolume(75);
-		component.mediaPlayer().media().play(urlVideo);
+		// component.mediaPlayer().audio().setVolume(75);
+		// component.mediaPlayer().media().play(urlVideo);
 	}
 	public static void main(String[] args) {
 		new VentanaEventos();
