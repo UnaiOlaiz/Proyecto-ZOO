e6d22204ec569bc634ec0c06705088af8bcb60a6
unaio <unaio@LaptopUnai>
2024-01-03 11:16:32 +0100

    Pequeños avances en la ventana de seguridad. Problemas con la configuración de hilos y todavía falta que funcionen los audios customizados.

--- ProyectoZooYarzabal3/src/Ventanas/VentanaSeguridad.java
+++ ProyectoZooYarzabal3/src/Ventanas/VentanaSeguridad.java
@@ -37,13 +37,14 @@ import javazoom.jl.player.Player;
 
 public class VentanaSeguridad extends JFrame {
 	protected JFrame vActual, vAnterior;
-	protected JButton btnVolver;
+	protected JButton btnVolver, botonEmpezarJornada;
 	protected JPanel pSur;
 	protected ArrayList<String> titulosAudios;
 	protected Thread hilo;
 	protected Player player;
-	protected boolean seguir = false;
+	protected boolean estaReproduciendose = false;
 	protected JTextArea area;
+
 	
 	/*
 	 * En esta ventana que se ejecutará tanto al registrar un nuevo trabajador con función de guardia de seguridad, o al 
@@ -53,21 +54,21 @@ public class VentanaSeguridad extends JFrame {
 	 * posible las órdenes y mensajes que recibe uno en la vida real.
 	 */
 	
-	public VentanaSeguridad(JFrame va) { // Luego aquí insertar JFrame va
+	public VentanaSeguridad() { // Luego aquí insertar JFrame va
 		super();
 		vActual = this;
-		vAnterior = va;
+//		vAnterior = va;
 		setTitle("Ventana de Seguridad");
-		setResizable(false);
+		setResizable(true);
 		setLocationRelativeTo(null);
-		setBounds(500, 300, 700, 350);
+		setBounds(500, 300, 800, 500);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         
         titulosAudios = new ArrayList<>();
     	// Primero rellenemos el array de títulos
     	titulosAudios.add("AudioLeon.mp3");
     	titulosAudios.add("AudioMono.mp3");
-//    	titulosAudios.add("TheoryOfEverything.mp3");
+    	titulosAudios.add("TheoryOfEverything.mp3");
     	
     	// Panel donde irán las explicaciones de como llevar a cabo el trabajo como guarda de seguridad del ZOO
     	JPanel panelNorte = new JPanel(new GridLayout(4,1));
@@ -106,101 +107,117 @@ public class VentanaSeguridad extends JFrame {
 	    panelNorte.add(panelExplicacion3);
 	    
 	    getContentPane().add(panelNorte, BorderLayout.NORTH);
+	    
+	    JPanel panelCentro = new JPanel();
+	    panelCentro.setBackground(new Color(70, 130, 180));
+	    ImageIcon icono = new ImageIcon("fotoRadio.png"); 
+        Image imagen = icono.getImage();
+        Image nuevaImagen = imagen.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
+        ImageIcon nuevoIcono = new ImageIcon(nuevaImagen);
+        JLabel labelImagen = new JLabel(nuevoIcono);
+        panelCentro.add(labelImagen);
+	    getContentPane().add(panelCentro, BorderLayout.CENTER);
     	
     	
-    	// Panel donde irá el botón para comenzar jornada
-    	JPanel panelCentro = new JPanel(new BorderLayout());
-    	
-    	JPanel panelBotones = new JPanel(new FlowLayout());
-        panelBotones.setBackground(new Color(70, 130, 180));
-    	JButton botonEmpezarJornada = new JButton( "ATENDER PROBLEMAS" );
-    	botonEmpezarJornada.setForeground(new Color(70, 130, 180));
-    	botonEmpezarJornada.setFont(new Font("Times New Roman", Font.BOLD, 14));
-    	panelBotones.add(botonEmpezarJornada);
-    	
-    	JButton botonAcabarJornada = new JButton( "SOLUCIONAR PROBLEMA" );
-    	botonAcabarJornada.setForeground(new Color(70, 130, 180));
-    	botonAcabarJornada.setFont(new Font("Times New Roman", Font.BOLD, 14));
-    	panelBotones.add(botonAcabarJornada);
-    	
-    	panelCentro.add(panelBotones, BorderLayout.NORTH);
-    	
-    	JPanel panelIntermedio = new JPanel();
-    	panelIntermedio.setLayout(new BoxLayout(panelIntermedio, BoxLayout.Y_AXIS));
-    	area = new JTextArea( 10, 40 );
-    	area.setForeground(new Color(70, 130, 180));
-    	area.setText("");
-    	area.setFont(new Font("Times New Roman", Font.BOLD, 14));
-    	panelIntermedio.add(area);
-    	panelCentro.add(panelIntermedio, BorderLayout.CENTER);
-    	
-    	getContentPane().add(panelCentro, BorderLayout.CENTER);
-    	
-    	// actionlistener de los botones
-    	botonEmpezarJornada.addActionListener(new ActionListener() {
-    	    @Override
-    	    public void actionPerformed(ActionEvent e) {
-    	        area.setText("");
-    	        if (titulosAudios.isEmpty()) {
-    	            JOptionPane.showMessageDialog(null, "NO HAY PROBLEMAS QUE SOLUCIONAR");
-    	            return;  // Salir del método si no hay más títulos de audios
-    	        }
-
-    	        String tituloSeleccionado = ElegirArchivoAleatorio(titulosAudios);
-    	        if (tituloSeleccionado == null) {
-    	            System.err.println("El título seleccionado es null");
-    	            return;  // Salir del método si el título seleccionado es null
-    	        }
-
-    	        titulosAudios.remove(tituloSeleccionado);  // Eliminar el título seleccionado de la lista
-
-    	        hilo = new Thread(() -> {
-    	            try {
-    	                botonEmpezarJornada.setEnabled(false);
-    	                FileInputStream fileInputStream = new FileInputStream(tituloSeleccionado);
-    	                // ... (resto del código para reproducir el archivo)
-    	            } catch (FileNotFoundException ex) {
-    	                System.err.println("Archivo no encontrado: " + tituloSeleccionado);
-    	                ex.printStackTrace();
-    	            } catch (Exception ex) {
-    	                System.err.println("Error al reproducir el archivo: " + tituloSeleccionado);
-    	                ex.printStackTrace();
-    	            } finally {
-    	                botonEmpezarJornada.setEnabled(true);
-    	            }
-    	        });
-
-    	        hilo.start();  // Iniciar el hilo
-    	    }
-    	});
-
-    	
-    	botonAcabarJornada.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				seguir = false;
-				if(player != null) {
-					area.setForeground(Color.GREEN);
-					area.append("PROBLEMA SOLUCIONADO" + "");
-					try {
-						Thread.sleep(100);
-					} catch (InterruptedException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					player.close();
-					hilo.interrupt();
-					botonEmpezarJornada.setEnabled(true);
-				}
-			}
-		});
+//    	// Panel donde irá el botón para comenzar jornada
+//    	JPanel panelCentro = new JPanel(new BorderLayout());
+//    	
+//    	JPanel panelBotones = new JPanel(new FlowLayout());
+//        panelBotones.setBackground(new Color(70, 130, 180));
+//    	JButton botonEmpezarJornada = new JButton( "ATENDER PROBLEMAS" );
+//    	botonEmpezarJornada.setForeground(new Color(70, 130, 180));
+//    	botonEmpezarJornada.setFont(new Font("Times New Roman", Font.BOLD, 14));
+//    	panelBotones.add(botonEmpezarJornada);
+//    	
+//    	JButton botonAcabarJornada = new JButton( "SOLUCIONAR PROBLEMA" );
+//    	botonAcabarJornada.setForeground(new Color(70, 130, 180));
+//    	botonAcabarJornada.setFont(new Font("Times New Roman", Font.BOLD, 14));
+//    	panelBotones.add(botonAcabarJornada);
+//    	
+//    	panelCentro.add(panelBotones, BorderLayout.NORTH);
+//    	
+//    	JPanel panelIntermedio = new JPanel();
+//    	panelIntermedio.setLayout(new BoxLayout(panelIntermedio, BoxLayout.Y_AXIS));
+//    	area = new JTextArea( 10, 40 );
+//    	area.setForeground(new Color(70, 130, 180));
+//    	area.setText("");
+//    	area.setFont(new Font("Times New Roman", Font.BOLD, 14));
+//    	panelIntermedio.add(area);
+//    	panelCentro.add(panelIntermedio, BorderLayout.CENTER);
+//    	
+//    	getContentPane().add(panelCentro, BorderLayout.CENTER);
+//    	
+//    	// actionlistener de los botones
+//    	botonEmpezarJornada.addActionListener(new ActionListener() {
+//    	    @Override
+//    	    public void actionPerformed(ActionEvent e) {
+//    	        area.setText("");
+//    	        if (titulosAudios.isEmpty()) {
+//    	            JOptionPane.showMessageDialog(null, "NO HAY PROBLEMAS QUE SOLUCIONAR");
+//    	            return;  // Salir del método si no hay más títulos de audios
+//    	        }
+//
+//    	        String tituloSeleccionado = ElegirArchivoAleatorio(titulosAudios);
+//    	        if (tituloSeleccionado == null) {
+//    	            System.err.println("El título seleccionado es null");
+//    	            return;  // Salir del método si el título seleccionado es null
+//    	        }
+//
+//    	        titulosAudios.remove(tituloSeleccionado);  // Eliminar el título seleccionado de la lista
+//
+//    	        hilo = new Thread(() -> {
+//    	            try {
+//    	                botonEmpezarJornada.setEnabled(false);
+//    	                FileInputStream fileInputStream = new FileInputStream(tituloSeleccionado);
+//    	                // ... (resto del código para reproducir el archivo)
+//    	            } catch (FileNotFoundException ex) {
+//    	                System.err.println("Archivo no encontrado: " + tituloSeleccionado);
+//    	                ex.printStackTrace();
+//    	            } catch (Exception ex) {
+//    	                System.err.println("Error al reproducir el archivo: " + tituloSeleccionado);
+//    	                ex.printStackTrace();
+//    	            } finally {
+//    	                botonEmpezarJornada.setEnabled(true);
+//    	            }
+//    	        });
+//
+//    	        hilo.start();  // Iniciar el hilo
+//    	    }
+//    	});
+//
+//    	
+//    	botonAcabarJornada.addActionListener(new ActionListener() {
+//			
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				// TODO Auto-generated method stub
+//				seguir = false;
+//				if(player != null) {
+//					area.setForeground(Color.GREEN);
+//					area.append("PROBLEMA SOLUCIONADO" + "");
+//					try {
+//						Thread.sleep(100);
+//					} catch (InterruptedException e1) {
+//						// TODO Auto-generated catch block
+//						e1.printStackTrace();
+//					}
+//					player.close();
+//					hilo.interrupt();
+//					botonEmpezarJornada.setEnabled(true);
+//				}
+//			}
+//		});
         
         // Panel donde irá el botón
+	    botonEmpezarJornada = new JButton( "ATENDER PROBLEMAS" );
+	    botonEmpezarJornada.setFont(new Font("Times New Roman", Font.BOLD, 14));
+        botonEmpezarJornada.setForeground(new Color(70, 130, 180));
         btnVolver = new JButton("VOLVER");
         btnVolver.setFont(new Font("Times New Roman", Font.BOLD, 14));
         btnVolver.setForeground(new Color(70, 130, 180));
+        JButton botonSolucionarProblemas = new JButton( "SOLUCIONAR PROBLEMAS" );
+        botonSolucionarProblemas.setFont(new Font("Times New Roman", Font.BOLD, 14));
+        botonSolucionarProblemas.setForeground(new Color(70, 130, 180));
         
         btnVolver.addActionListener((e)->{
         	vAnterior.setVisible(true);
@@ -209,7 +226,66 @@ public class VentanaSeguridad extends JFrame {
         pSur = new JPanel();
         pSur.setBackground(new Color(70, 130, 180));
         getContentPane().add(pSur, BorderLayout.SOUTH);
+        pSur.add(botonEmpezarJornada);
+        pSur.add(botonSolucionarProblemas);
         pSur.add(btnVolver);
+        
+        botonSolucionarProblemas.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if (estaReproduciendose) {
+					return;
+				}
+				if (player != null) {
+					player.close();
+					player = null;
+				}
+			}
+		});
+        
+        botonEmpezarJornada.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				estaReproduciendose = true;
+				botonEmpezarJornada.setEnabled(false);
+				/*
+				 * Vamos a esperar un rato de tiempo aleatorio (poco tiempo) hasta que suene la alarma y
+				 * salga la alerta de que hay un problema, luego se reproducirá el audio
+				 */
+				Random random = new Random();
+				int tiempoAleatorio = random.nextInt(5000) + 1000;
+				
+				try {
+					Thread.sleep(tiempoAleatorio);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				if (!titulosAudios.isEmpty()) {
+					// Que se reproduzca el audio de alarma
+					try {
+						ReproducirAudioMp3("SonidoAlarma.mp3");
+					} catch (JavaLayerException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				
+				String audioAleatorio = ElegirArchivoAleatorio(titulosAudios);
+				try {
+					ReproducirAudioMp3(audioAleatorio);
+				} catch (JavaLayerException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				titulosAudios.remove(audioAleatorio);
+				System.out.println(audioAleatorio);
+				botonEmpezarJornada.setEnabled(true);
+			}
+		});
         
         setVisible(true);
 		
@@ -231,13 +307,9 @@ public class VentanaSeguridad extends JFrame {
 	            switch (tituloAleatorio) {
 	                case "AudioLeon.mp3":
 	                    JOptionPane.showMessageDialog(null, "¡ALERTA CÓDIGO 14!", "Alerta", JOptionPane.WARNING_MESSAGE);
-	                    area.setForeground(Color.RED);
-	                    area.append("ALERTA CÓDIGO 14" + "\n" + "VISITANTE EN APUROS, SE REQUIERE AYUDA INMEDIATA" + "\n");
 	                    break;
 	                case "AudioMono.mp3":
-	                    area.setForeground(Color.RED);
 	                    JOptionPane.showMessageDialog(null, "¡ALERTA CÓDIGO 12!", "Alerta", JOptionPane.WARNING_MESSAGE);
-	                    area.append("ALERTA CÓDIGO 12" + "\n" + "ANIMAL SE HA ESCAPADO, SUELTO POR EL ZOO" + "\n");
 	                    break;
 	                default:
 	                    break;
@@ -258,8 +330,11 @@ public class VentanaSeguridad extends JFrame {
 		if (nombreArchivo != null) {
 			try {
 				FileInputStream fileInputStream = new FileInputStream(nombreArchivo);
-				Player player = new Player(fileInputStream);
+				player = new Player(fileInputStream);
 				player.play();
+				player.close();
+				estaReproduciendose = false;
+				botonEmpezarJornada.setEnabled(true);
 			}catch (FileNotFoundException e) {
 				// TODO: handle exception
 				e.printStackTrace();
@@ -269,8 +344,10 @@ public class VentanaSeguridad extends JFrame {
 		}
 	}
 	
-//	public static void main(String[] args) {
-//		new VentanaSeguridad();
-//	}
+	
+	
+	public static void main(String[] args) {
+		new VentanaSeguridad();
+	}
 
 }
